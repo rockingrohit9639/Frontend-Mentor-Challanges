a2e9d40c5796bd31e5a16055b0c0770b569d2d67
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-09 10:39:05 +0530

    added desktop share

--- sites/article-preview-component-master/script.js
+++ sites/article-preview-component-master/script.js
@@ -1,10 +1,17 @@
 const shareButtons = document.querySelector(".share__buttons");
 const mobileShareButton1 = document.querySelector(".mobile__share1");
 const mobileShareButton2 = document.querySelector(".mobile__share2");
+const desktopShareButton = document.querySelector(".desktop__shareButton");
+const desktopShare = document.querySelector(".desktop__share");
 
 mobileShareButton1.addEventListener("click", handleMobileShareToggle);
 mobileShareButton2.addEventListener("click", handleMobileShareToggle);
+desktopShareButton.addEventListener("click", handleMobileShareToggle);
 
 function handleMobileShareToggle() {
   shareButtons.classList.toggle("active");
+}
+
+function handleMobileShareToggle() {
+  desktopShare.classList.toggle("active");
 }
